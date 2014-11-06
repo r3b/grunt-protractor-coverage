c62ea521ecae867ebdafae93a2d47cf2510d9f2f
Merrifield, Jay <jmerrifiel@gannett.com>
2014-11-06 18:31:44 -0500

    Added ability to customize the collector port through options

--- tasks/protractor_coverage.js
+++ tasks/protractor_coverage.js
@@ -102,6 +102,7 @@ module.exports = function(grunt) {
       keepAlive: true,
       noColor: false,
       debug: false,
+      collectorPort: 3001,
       args: {}
     });
     var saveCoverageTemplate = grunt.file.expand(["resources/saveCoverage.tmpl", "node_modules/grunt-protractor-coverage/resources/saveCoverage.tmpl", process.cwd()+'/**/resources/saveCoverage.tmpl']).shift();
@@ -179,19 +180,19 @@ module.exports = function(grunt) {
     grunt.verbose.writeln("Spawn node with arguments: " + args);
     // start the collector
     var collector=require('coverage-collector');
-    collector(3001);
+    collector({port: opts.collectorPort});
     function cleanup(callback){
       suppliedArgs.specs.forEach(function(f){grunt.file.delete(f, {force:true});});
     }
     function getCoverageData(callback){
-        http.get("http://localhost:3001/data", function(res) {
+        http.get("http://localhost:" + opts.collectorPort + "/data", function(res) {
           var payload="";
           res.on('data', function (chunk) {
             payload+=chunk;
             // grunt.log.warn('BODY: ' + chunk);
           });
           res.on('end', function(){
-            http.get("http://localhost:3001/done", function(res) {
+            http.get("http://localhost:" + opts.collectorPort + "/done", function(res) {
               cleanup();
               if(callback){callback(payload);}
             })
